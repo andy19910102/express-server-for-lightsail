ff6bbb9539b2af34fe2b48908121ef602b41a909
andy19910102 <andy19910102@gmail.com>
2023-08-02 21:19:21 +0800

    remove mysql.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const admin = require("../firebase");
-const db = require("../mysql");
+// const db = require("../mysql");
 
 router.post("/login", (req, res, next) => {
     const idToken = req.body.idToken;
@@ -37,61 +37,61 @@ router.get('/user/:userId', (req, res, next) => {
 });
 
 // 取得所有產品
-router.get('/product', (req, res ,next) => {
-    db.query("SELECT * FROM products", (err, products) => {
-        if (err) throw err;
-        console.log("products:", products);
-        res.status(200).json(products);
-    }); 
-});
+// router.get('/product', (req, res ,next) => {
+//     db.query("SELECT * FROM products", (err, products) => {
+//         if (err) throw err;
+//         console.log("products:", products);
+//         res.status(200).json(products);
+//     }); 
+// });
 
 // 取得單一產品
-router.get('/product/:id', (req, res ,next) => {
-    const id = req.params.id;
-    db.query("SELECT * FROM products WHERE id = ?", [id], (err, products) => {
-        if (err) throw err;
-        console.log("products:", products);
-        res.status(200).json(products[0])
-    });
-});
+// router.get('/product/:id', (req, res ,next) => {
+//     const id = req.params.id;
+//     db.query("SELECT * FROM products WHERE id = ?", [id], (err, products) => {
+//         if (err) throw err;
+//         console.log("products:", products);
+//         res.status(200).json(products[0])
+//     });
+// });
 
 // 新增一個產品
-router.post('/product', (req, res ,next) => {
-    const product = req.body;
-    db.query("INSERT INTO products SET ?", [product], (err, result) => {
-        if (err) throw err;
-        console.log("result:", result);
-        res.status(200).json({
-            newProductId: result.insertId,
-            msg: "OK"
-        });
-    });
-});
+// router.post('/product', (req, res ,next) => {
+//     const product = req.body;
+//     db.query("INSERT INTO products SET ?", [product], (err, result) => {
+//         if (err) throw err;
+//         console.log("result:", result);
+//         res.status(200).json({
+//             newProductId: result.insertId,
+//             msg: "OK"
+//         });
+//     });
+// });
 
 // 更新一個產品
-router.put('/product/:id', (req, res ,next) => {
-    const product = req.body;
-    const id = req.params.id;
-    db.query("UPDATE products SET ? WHERE id = ?", [product, id], (err, result) => {
-        if (err) throw err;
-        console.log("result:", result);
-        res.status(200).json({
-            msg: "OK"
-        });
-    });
-});
+// router.put('/product/:id', (req, res ,next) => {
+//     const product = req.body;
+//     const id = req.params.id;
+//     db.query("UPDATE products SET ? WHERE id = ?", [product, id], (err, result) => {
+//         if (err) throw err;
+//         console.log("result:", result);
+//         res.status(200).json({
+//             msg: "OK"
+//         });
+//     });
+// });
 
 // 刪除一個產品
-router.delete('/product/:id', (req, res ,next) => {
-    const id = req.params.id;
-    db.query("DELETE FROM products WHERE id = ?", [id], (err, result) => {
-        if (err) throw err;
-        console.log("result: ", result);
-        res.status(200).json({
-            msg: "OK"
-        });
-    });
-});
+// router.delete('/product/:id', (req, res ,next) => {
+//     const id = req.params.id;
+//     db.query("DELETE FROM products WHERE id = ?", [id], (err, result) => {
+//         if (err) throw err;
+//         console.log("result: ", result);
+//         res.status(200).json({
+//             msg: "OK"
+//         });
+//     });
+// });
 
 
 module.exports = router;
